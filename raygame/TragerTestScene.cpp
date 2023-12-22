0084986df9d685e44dd68fe74c7f836ec8354a66
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2023-12-22 11:53:45 -0600

    Enemy created and given movement

--- raygame/TragerTestScene.cpp
+++ raygame/TragerTestScene.cpp
@@ -1,11 +1,23 @@
 #include "TragerTestScene.h"
 #include "Character.h"
+#include "Enemy.h"
 #include <iostream>
+#include "Transform2D.h"
+#include "Movement.h"
 
 using namespace std;
 
 void TragerTestScene::start()
 {
+	Enemy* enemy = new Enemy();
+	MovementComponent* enemyMove = (MovementComponent*)enemy->addComponent(new MovementComponent(50, enemy));;
+	enemyMove->setVelocity({ 50, 50 });
+
+	MathLibrary::Vector2 scale = MathLibrary::Vector2(50, 50);
+
+	enemy->getTransform()->setScale(scale);
+	addActor(enemy);
+
 	Character* test = new Character(50, nullptr);
 	cout << test->getHealth();
 }
